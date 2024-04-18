303d460c24996d358838ed7dd1fc6a8b2966d6a0
adharsh <adharsh@maptell.com>
2024-04-18 13:35:36 +0530

    get all users controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,20 @@
-import mongoose from 'mongoose'
 import { User } from '../models/user.model.js'
 
-const getAllUser = () => {
-    
+// @desc get all users
+// route GET /api/v1/users/get-all-users
+const getAllUsers = async () => {
+    try {
+        const users = await User.find()
+        if (users.length > 0) {
+            res.json({ status: 200, message: 'Users Retrieved Successfully', data: users });
+        }
+        else {
+            res.json({ status: 200, message: 'No users found', data: [] });
+        }
+    }
+    catch (error) {
+        res.json({ status: 500, message: 'Server Error' });
+    }
 }
 
-export { getAllUser }
+export { getAllUsers }
--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,5 +1,5 @@
 import Router from 'express'
-import { getAllUser } from '../controllers/user.controller.js'
+import { getAllUsers } from '../controllers/user.controller.js'
 
 const userRouter = Router()
 
