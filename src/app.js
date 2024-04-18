770911b1b14dd122cad838a991191d82a252e218
adharsh <adharsh@maptell.com>
2024-04-18 15:44:12 +0530

    all routes connected with server (app.js)

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import express from 'express'
 import cors from 'cors'
+import userRouter from './routers/user.router.js'
+import courseRouter from './routers/course.router.js'
+import orderRouter from './routers/order.router.js'
 
 const app = express()
 
@@ -7,5 +10,9 @@ app.use(cors())
 app.use(express.json({ limit: '16kb' }))
 app.use(express.urlencoded({ extended: true, limit: '16kb' }))
 app.use(express.static('public'))
+
+app.use('/api/v1/user', userRouter)
+app.use('/api/v1/course', courseRouter)
+app.use('/api/v1/order', orderRouter)
 
 export { app }
--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -3,4 +3,4 @@ import { getAllUsers } from '../controllers/user.controller.js'
 
 const userRouter = Router()
 
-userRouter.route('/get-all-users', getAllUser)
+userRouter.route('/get-all-users', getAllUsers)
