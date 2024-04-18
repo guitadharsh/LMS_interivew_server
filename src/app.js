b25b40265a314dd8c094736567c1da45ef6bd5a3
adharsh <adharsh@maptell.com>
2024-04-18 12:30:00 +0530

    app.js public static

--- src/app.js
+++ src/app.js
@@ -6,3 +6,6 @@ const app = express()
 app.use(cors())
 app.use(express.json({ limit: '16kb' }))
 app.use(express.urlencoded({ extended: true, limit: '16kb' }))
+app.use(express.static('public'))
+
+export { app }
