bc339012b5cbd88247948d7dfec310c23f34b70d
adharsh <adharshd100@gmail.com>
2024-04-19 18:17:43 +0530

    cors updated

--- app.js
+++ app.js
@@ -6,7 +6,18 @@ import { orderRouter } from './src/routers/order.router.js'
 
 const app = express()
 
-app.use(cors())
+//cors restriction
+const allowedOrigins = [process.env.DEVELOPMENT_ORIGIN, process.env.PRODUCTION_ORIGIN];
+app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS')); 
+      }
+    }
+  }));
+
 app.use(express.json({ limit: '16kb' }))
 app.use(express.urlencoded({ extended: true, limit: '16kb' }))
 app.use(express.static('public'))
