24f876f676cae07c92d05cebb2fb8b762f78fb32
adharsh <adharshd100@gmail.com>
2024-04-19 18:03:36 +0530

    server app http to express

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import dotenv from 'dotenv'
 import { app } from './app.js'
-import http from 'http'
 import connectMongoDb from './src/config/mongodb.js'
 
 dotenv.config({
@@ -8,10 +7,9 @@ dotenv.config({
 })
 
 const PORT = process.env.PORT || 8000;
-const server = http.createServer(app);
 
 connectMongoDb()
     .then(() => {
-        server.listen(PORT, () => console.log(`server connected @ ${PORT}`))
+        app.listen(PORT, () => console.log(`server connected @ ${PORT}`))
     })
     .catch((err) => console.log(`server faied ${err}`))
